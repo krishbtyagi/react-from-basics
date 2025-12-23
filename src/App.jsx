b1a394b5638638922bcc3d7dc5064ca9222f484d
Krish Bhollar Tyagi <krishbtyagi@gmail.com>
2025-12-23 17:32:51 +0530

    first commit, did basic components

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+function App() {
+
+  return (
+    <div style={{background: "#dfe6e9", height:"100vh", }}>
+      <div style={{display: "flex", justifyContent: "center"}}>
+      <PostComponent />
+    </div>
+    </div>
+  )
+}
+
+const style = {width: 200, backgroundColor: "white", bortderRadius : 10, borderColor : "gray", borderWidth: 1, padding: 20 }
+
+function PostComponent() {
+  return <div style={style}> 
+    <div style={{display: "flex"}}>
+      <img src={"https://media.licdn.com/dms/image/v2/D5603AQF1kRvp…eta&t=D3jp4T_Z7s-zDV5gwrAyIVwv7KBxH2ZbrMCFkQnu0WE"} style={{
+        width: 30,
+        height: 30,
+        borderRadius :20 
+      }} />
+      <div style= {{fontSize: 10, marginLeft: 10}}>
+        <b>
+          krish
+        </b>
+        <div>69 followers</div>
+        <div>20m</div>
+      </div>
+    </div>
+    <div style={{fontSize: 12}} >
+      I am pleased to announce that...
+    </div>
+</div>
+}
+
+export default App
